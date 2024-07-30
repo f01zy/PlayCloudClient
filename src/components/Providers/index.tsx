284cdf62e8804503f12717bdd0592dbb838857dc
f01zy <aminovali955@gmail.com>
2024-07-30 13:11:31 +0400

    commit

--- src/components/Providers/index.tsx
+++ src/components/Providers/index.tsx
@@ -1,14 +1,8 @@
 "use client"
 
-import { muiTheme } from "@/service/theme.service";
 import { store } from "@/store/store";
-import { ThemeProvider } from "@mui/material";
 import { Provider } from "react-redux";
 
 export const Providers = ({ children }: { children: React.ReactNode }) => {
-  return (
-    <ThemeProvider theme={muiTheme}>
-      <Provider store={store}>{children}</Provider>
-    </ThemeProvider>
-  )
+  return <Provider store={store}>{children}</Provider>;
 }
