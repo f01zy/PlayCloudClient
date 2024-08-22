4be462c52fefc6d41b9c4b86c594861ceb2f8d05
f01zy <aminovali955@gmail.com>
2024-08-22 20:10:32 +0400

    optimization

--- src/app/music/[id]/page.tsx
+++ src/app/music/[id]/page.tsx
@@ -1,15 +1,15 @@
 import { IMusic } from "@/interfaces/music.interface";
 import MusicDetail from "@/page/MusicDetail";
 import { Metadata } from "next";
+import { notFound } from "next/navigation";
 
 export const generateMetadata = async ({ params }: { params: { id: string } }): Promise<Metadata> => {
   const id = params.id
   const music = await fetch(`http://147.45.160.178:5050/api/music/${id}`).then(res => res.json()).then(res => res as IMusic | null)
 
-  if (!music || music._id) return {
-    title: "Music not found | PlayCloud",
-    description: "Music not found. Try pass a valid music id"
-  }
+  console.log(music)
+
+  if (!music) return notFound()
 
   return {
     title: music.name + " | PlayCloud",
