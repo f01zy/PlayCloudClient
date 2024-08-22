fcdea3e95cbbae35ca183f26767f1a4cc08d562f
f01zy <aminovali955@gmail.com>
2024-08-22 20:02:06 +0400

    optimization

--- src/app/music/[id]/page.tsx
+++ src/app/music/[id]/page.tsx
@@ -7,8 +7,8 @@ export const generateMetadata = async ({ params }: { params: { id: string } }): 
   const music = await fetch(`http://147.45.160.178:5050/api/music/${id}`).then(res => res.json()).then(res => res as IMusic | null)
 
   return {
-    title: music ? music.name : "Music not found" + " | PlayCloud",
-    description: music ? `A separate music page called ${music.name}. The author of the song is ${music.author.username}` : "Music not found. Try pass a valid music id",
+    title: (music ? music.name : "Music not found") + " | PlayCloud",
+    description: music ? `A separate music page called ${music.name}` : "Music not found. Try pass a valid music id",
   }
 }
 
--- src/app/profile/[id]/page.tsx
+++ src/app/profile/[id]/page.tsx
@@ -7,7 +7,7 @@ export const generateMetadata = async ({ params }: { params: { id: string } }): 
   const user = await fetch(`http://147.45.160.178:5050/api/users/${id}`).then(res => res.json()).then(res => res as IUser | null)
 
   return {
-    title: user ? user.username : "User not found" + " | PlayCloud",
+    title: (user ? user.username : "User not found") + " | PlayCloud",
     description: user ? `The ${user.username} page. Here you can get acquainted with his profile and watch his tracks` : "User not found. Try pass a valid user id",
   }
 }
