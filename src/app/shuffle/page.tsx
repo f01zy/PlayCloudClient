1b9fd2e1ce6f67c5d21f351f3d4d84b3328445a7
f01zy <aminovali955@gmail.com>
2024-08-25 01:22:57 +0400

    commit

--- src/app/shuffle/page.tsx
+++ src/app/shuffle/page.tsx
@@ -10,11 +10,13 @@ const ShufflePage: NextPage = () => {
   const [music, setMusic] = useState<IMusic>()
   const router = useRouter()
 
-  getAllMusic()
-    .then(res => setMusic(res[Math.floor(Math.random() * (res.length - 0)) + 0]))
-    .then(() => {
-      if (music) return router.push("/music/" + music._id)
-    })
+  useEffect(() => {
+    getAllMusic()
+      .then(res => setMusic(res[Math.floor(Math.random() * (res.length - 0)) + 0]))
+      .then(() => {
+        if (music) return router.push("/music/" + music._id)
+      })
+  }, [])
 
   return <div></div>
 }
