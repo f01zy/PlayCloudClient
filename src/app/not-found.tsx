9d0b2fd35580ff059821be167c5f22260c8d435e
f01zy <aminovali955@gmail.com>
2024-08-25 01:45:30 +0400

    commit

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -5,7 +5,7 @@ import { useRouter } from "next/navigation";
 const NotFound = () => {
   const router = useRouter()
 
-  return <main className="w-full h-full flex items-center justify-center">
+  return <main className="flex items-center justify-center screen-block">
     <div className="text-center">
       <h1 className="text-xl">There was a problem</h1>
       <p className="mt-2 text-base">Go <span className="!text-blue-500" onClick={() => router.back()}>back</span> to the previous page</p>
