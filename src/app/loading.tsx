e2859ba2922e6231ec902a1a6f08d3adc67bda4c
f01zy <aminovali955@gmail.com>
2024-08-25 01:42:47 +0400

    commit

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -1,7 +1,7 @@
 "use client"
 
 const Loading = () => {
-  return <main className="w-full h-full flex items-center justify-center">
+  return <main className="flex items-center justify-center screen-block">
     <div className="text-center">
       <h1 className="text-xl">Loading</h1>
       <p className="mt-2 text-base">Loading is underway. Please wait</p>
