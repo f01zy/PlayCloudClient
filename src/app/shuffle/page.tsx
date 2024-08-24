b8944780e35d5b7717bf3c2eb2e9f4b8db1825d4
f01zy <aminovali955@gmail.com>
2024-08-25 01:15:05 +0400

    commit

--- src/app/shuffle/page.tsx
+++ src/app/shuffle/page.tsx
@@ -1,28 +1,20 @@
 "use client"
 
-import { useTypedSelector } from "@/hooks/selector.hook";
 import { IMusic } from "@/interfaces/music.interface";
-import { AppDispatch } from "@/store/store";
 import { getAllMusic } from "@/utils/getAllMusic.utils";
-import { playMusic } from "@/utils/playMusic.utils";
 import { NextPage } from "next";
 import { useRouter } from "next/navigation";
 import { useEffect, useState } from "react";
-import { useDispatch } from "react-redux";
 
 const ShufflePage: NextPage = () => {
   const [music, setMusic] = useState<IMusic>()
   const router = useRouter()
-  const dispatch = useDispatch<AppDispatch>()
-  const user = useTypedSelector(selector => selector.userSlice.user)
 
-  useEffect(() => {
-    getAllMusic()
-      .then(res => setMusic(res[Math.floor(Math.random() * (res.length - 0)) + 0]))
-      .then(() => {
-        if (user && music) { playMusic({ ...music }, dispatch, user); return router.push("/music/" + music._id) }
-      })
-  }, [])
+  getAllMusic()
+    .then(res => setMusic(res[Math.floor(Math.random() * (res.length - 0)) + 0]))
+    .then(() => {
+      if (music) return router.push("/music/" + music._id)
+    })
 
   return <div></div>
 }
