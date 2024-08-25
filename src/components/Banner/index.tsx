67be63ac54f47bd2171c00a2d6f8d48e8dd663a8
f01zy <aminovali955@gmail.com>
2024-08-25 23:23:19 +0400

    commit

--- src/components/Banner/index.tsx
+++ src/components/Banner/index.tsx
@@ -1,13 +1,16 @@
 import styles from "@/components/Banner/styles.module.scss"
+import Image from "next/image";
 import { FC } from "react";
 
 interface IBanner {
   title: string,
-  value: string
+  value: string,
+  image: string
 }
 
-const Banner: FC<IBanner> = ({ title, value }) => {
+const Banner: FC<IBanner> = ({ title, value, image }) => {
   return <div className={styles.banner}>
+    <Image src={image} alt="banner" width={100} height={100} />
     <div className={styles.content}>
       <h2>{title}</h2>
       <p>{value}</p>
