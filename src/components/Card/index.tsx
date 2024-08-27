47fa48e9e42833aca37c82ab77df04fc01f602a5
f01zy <aminovali955@gmail.com>
2024-08-27 20:30:13 +0400

    commit

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -22,8 +22,8 @@ const Card: FC<IMusic> = ({ author, name, listenings, _id, likes, date, type }) 
 	const user = useTypedSelector(selector => selector.userSlice.user)
 	const dispatch = useDispatch<AppDispatch>()
 
-	return <div className={styles.card}>
-		<DraggableWrapper>
+	return <DraggableWrapper>
+		<div className={styles.card}>
 			<div className={styles.container}>
 				<div className={styles.cover}>
 					<Image src={`${SERVER_URL}/cover/${_id}.jpg`} alt={name} width={100} height={100} />
@@ -40,8 +40,8 @@ const Card: FC<IMusic> = ({ author, name, listenings, _id, likes, date, type }) 
 				<p>({filterListeningsByDate(listenings).length} listening on last week)</p>
 				<Link href={`/profile/${author._id}`}><p>{author.username}</p></Link>
 			</div>
-		</DraggableWrapper>
-	</div>
+		</div>
+	</DraggableWrapper>
 }
 
 export default Card;
