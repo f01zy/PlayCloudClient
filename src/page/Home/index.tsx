2ae35d01048f09bc0c0cd57d3ad38e243fad9ca1
f01zy <aminovali955@gmail.com>
2024-08-27 15:56:55 +0400

    commit

--- src/page/Home/index.tsx
+++ src/page/Home/index.tsx
@@ -8,7 +8,7 @@ import styles from "@/page/Home/styles.module.scss"
 const Home = () => {
   return <div className={styles.home}>
     <Banner title="PlayCloud" value="The best platform for high-quality music listening" />
-    <TracksGrid label="Most popular" quantity={6} sort="listening" />
+    <TracksGrid label="Most popular" quantity={6} sort="listenings" />
   </div>
 }
 
