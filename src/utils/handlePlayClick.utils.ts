e082d5ed5b2c347ed25d62d8f8d89abe743b04c3
f01zy <aminovali955@gmail.com>
2024-09-27 22:59:04 +0400

    commit

--- src/utils/handlePlayClick.utils.ts
+++ src/utils/handlePlayClick.utils.ts
@@ -11,14 +11,19 @@ import { playMusic } from "./playMusic.utils";
 import { IPlaylist } from "@/interfaces/playlist.interface";
 import { setPlaylist } from "@/store/playlist/playlist.slice";
 
+const pause = (dispatch: Dispatch<UnknownAction>) => {
+  if (musicInterval) clearInterval(musicInterval); playerController.pause(); dispatch(setIsPaused(true))
+}
+
 export const handlePlayClick = (dispatch: Dispatch<UnknownAction>, composition: IMusic | IPlaylist, user: IUser | null, router: AppRouterInstance, id: string | undefined) => {
   if (!user) return router.push("/login")
   if (composition.type === "playlist" && composition.tracks.length === 0) return alert("This playlist is empty")
   if (id != composition._id) {
+    pause(dispatch)
     composition.type === "playlist" ? dispatch(setPlaylist(composition._id)) : dispatch(setPlaylist(null))
     playMusic(composition, dispatch, user)
     return
   }
   if (playerController.getIsPaused) { startMusicInterval(dispatch); playerController.resume(); dispatch(setIsPaused(false)) }
-  else { if (musicInterval) clearInterval(musicInterval); playerController.pause(); dispatch(setIsPaused(true)) }
+  else pause(dispatch)
 }
