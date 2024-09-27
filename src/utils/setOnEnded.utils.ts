6a735b032a348d36f3bc0cd832cd8684329c825c
f01zy <aminovali955@gmail.com>
2024-09-27 23:54:50 +0400

    commit

--- src/utils/setOnEnded.utils.ts
+++ src/utils/setOnEnded.utils.ts
@@ -7,14 +7,15 @@ import { playMusic } from "./playMusic.utils";
 import { IMusic } from "@/interfaces/music.interface";
 import { random } from './random.utils';
 import { IPlaylist } from '@/interfaces/playlist.interface';
+import { notFound } from 'next/navigation';
 
 export const setOnEnded = async (user: IUser, currentMusic: IMusic, dispatch: Dispatch<UnknownAction>, id: string | undefined = undefined) => {
-  const findPlaylist = await $api.get<IPlaylist>(`/playlist/${id}`).then(res => res.data)
-  let playlist = id ? findPlaylist.tracks : user.history
-  if (!playlist) playlist = user.history
+  const find = id ? await $api.get<IPlaylist>(`/playlist/${id}`).then(res => res.data) : null
+  let playlist = find ? find.tracks : user.history
+  if (!playlist) return notFound()
   const music = getMusicMode() === "random" ?
     playlist[random(0, playlist.length - 1)] :
     currentMusic
   console.log(playlist, music)
-  playerController.onEnded = () => { id ? playMusic(findPlaylist!, dispatch, user) : playMusic(music, dispatch, user) };
+  playerController.onEnded = () => { id ? playMusic(find as any, dispatch, user) : playMusic(music, dispatch, user) };
 }
