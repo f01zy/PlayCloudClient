8d6e1287c48bc692859ddc923d63d4a4d852f2e0
f01zy <aminovali955@gmail.com>
2024-09-27 23:47:41 +0400

    commit

--- src/utils/setOnEnded.utils.ts
+++ src/utils/setOnEnded.utils.ts
@@ -1,14 +1,17 @@
+import { alert } from './alert.utils';
+import { $api } from '@/http';
 import { Dispatch, UnknownAction } from '@reduxjs/toolkit';
 import { getMusicMode, playerController } from "@/components/Wrappers/Layout";
 import { IUser } from "@/interfaces/user.interface";
 import { playMusic } from "./playMusic.utils";
 import { IMusic } from "@/interfaces/music.interface";
 import { random } from './random.utils';
+import { IPlaylist } from '@/interfaces/playlist.interface';
 
-export const setOnEnded = (user: IUser, currentMusic: IMusic, dispatch: Dispatch<UnknownAction>, id: string | undefined = undefined) => {
-  const findPlaylist = user.playlists.find(playlist => playlist._id === id)
-  let playlist = id ? findPlaylist?.tracks : user.history
-  if (!playlist) return
+export const setOnEnded = async (user: IUser, currentMusic: IMusic, dispatch: Dispatch<UnknownAction>, id: string | undefined = undefined) => {
+  const findPlaylist = await $api.get<IPlaylist>(`/playlist/${id}`).then(res => res.data)
+  let playlist = id ? findPlaylist.tracks : user.history
+  if (!playlist) playlist = user.history
   const music = getMusicMode() === "random" ?
     playlist[random(0, playlist.length - 1)] :
     currentMusic
